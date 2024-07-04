e889319d7b406e7e33317bd822cab99386dacdf1
AnkitindiiitA <ankitsharma.ind.2001@gmail.com>
2024-07-04 19:16:35 +0530

    initial commit

---
+++ Board.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    public int size;
+    public PlayingPiece[][]board;
+
+    public Board(int size) {
+        this.size = size;
+        board = new PlayingPiece[size][size];
+    }
+
+
+    public boolean addPiece(int row, int column, PlayingPiece playingPiece) {
+
+        if(board[row][column] != null) {
+            return false;
+        }
+        board[row][column] = playingPiece;
+        return true;
+    }
+
+
+    public ArrayList<ArrayList<Integer>> getFreeCells() {
+        ArrayList<ArrayList<Integer> > freeCells = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == null) {
+                    ArrayList<Integer> rowColumn = new ArrayList<>();
+                    rowColumn.add(i);
+                    rowColumn.add(j);
+                    freeCells.add(rowColumn);
+                }
+            }
+        }
+
+        return freeCells;
+    }
+
+    public void printBoard() {
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != null) {
+                    System.out.print(board[i][j].pieceType.name() + "   ");
+                } else {
+                    System.out.print("    ");
+
+                }
+                System.out.print(" | ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String args[]) {
+        TicTacToeGame game = new TicTacToeGame();
+        game.initializeGame();
+        System.out.println("game winner is: " + game.startGame());
+    }
+}
+
+
